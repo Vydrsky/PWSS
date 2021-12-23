f10393729f9d58ba2582177cbeefd95d1c16e6f4
Vydrsky <vydrsky@gmail.com>
2021-12-23 16:27:48 +0100

    update

--- PWSS/GetServiceType.cpp
+++ PWSS/GetServiceType.cpp
@@ -6,8 +6,20 @@ void GetServiceType::Enter()
 {
 	
 	for (auto it = stateMachine->data->socketList.begin(); it != stateMachine->data->socketList.end(); ++it) {
-		char bufor[1024] = "UPLOAD\nabcdsgd";
-		stateMachine->data->fileMap[(*it).fd].ReadInput(bufor);
+		
+		int amountRecieved = 0;
+		char buff[1024];
+		int dataRecieved = 0;
+		recv((*it).fd, buff + dataRecieved, 8, 0);
+		do {
+			amountRecieved = recv((*it).fd, buff + dataRecieved, 1024, 0);
+			dataRecieved += amountRecieved;
+		} while (amountRecieved > 0 && dataRecieved > 0);
+		stateMachine->data->fileMap[(*it).fd].ReadInput(buff);
+		for (size_t i = 0;buff[i]!=0 ; i++)
+		{
+			cout << buff[i];
+		};
 	}
 }
 
--- PWSS/main.cpp
+++ PWSS/main.cpp
@@ -13,13 +13,10 @@ int main()
     ServerData data;
 
     stateMachine.Init(new ConfigureServer(&stateMachine),&data);
-    FileAction fileAction;
-    char buf[1024] = "UPLOAD\nGFDSHdfghbjfgdhbagfdhbjhjdfaghjdfg";
-    fileAction.ReadInput(buf);
-    cout << fileAction.GetCommand() << endl;
-    cout << fileAction.GetBuff() << endl;
     while (1) {
         stateMachine.ChangeState(new CheckForClients(&stateMachine));
+        stateMachine.Tick();
+        stateMachine.ChangeState(new GetServiceType(&stateMachine));
         stateMachine.Tick();
     }
     cout << stateMachine.data->serverSocket;
